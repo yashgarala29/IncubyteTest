a93cc4fe6e8d290695fbe6809f37564533c874d1
Yash Garala <yashgarala29@gmail.com>
2024-02-17 12:18:51 +0530

    test added

--- src/test/java/org_test/example/CalculateTDDTest.java
+++ src/test/java/org_test/example/CalculateTDDTest.java
@@ -10,6 +10,17 @@ public class CalculateTDDTest {
         CalculateTDD calculateTDDTest=new CalculateTDD();
         int result=calculateTDDTest.add("");
         assertEquals(0,result);
-
+    }
+    @Test
+    public void test2() {
+        CalculateTDD calculateTDDTest=new CalculateTDD();
+        int result=calculateTDDTest.add("1");
+        assertEquals(1,result);
+    }
+    @Test
+    public void test3() {
+        CalculateTDD calculateTDDTest=new CalculateTDD();
+        int result=calculateTDDTest.add("1,5");
+        assertEquals(6,result);
     }
 }
